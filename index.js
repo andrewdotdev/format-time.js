d075b1313dddefe405ba7db1c226eb45f252e580
Andrespolicrack <andrespoli071009@gmail.com>
2024-01-14 02:04:27 +0100

    Bugs Fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function parseMs(ms) {
     if (typeof ms !== 'number') {
         throw new TypeError('Se esperaba un número');
     }
-  
+
     return {
         days: Math.trunc(ms / 86400000),
         hours: Math.trunc(ms / 3600000) % 24,
@@ -11,20 +11,20 @@ function parseMs(ms) {
         seconds: Math.trunc(ms / 1000) % 60,
         ms: Math.trunc(ms) % 1000
     };
-  }
-  
-  // Adaptado de https://github.com/rafaelrinaldi/add-zero
-  function addZero(time, digits = 2) {
+}
+
+// Adaptado de https://github.com/rafaelrinaldi/add-zero
+function addZero(time, digits = 2) {
     let paraString = time.toString();
     digits = digits || 2;
-  
+
     let capacity = digits - paraString.length + 1;
     let result = new Array(capacity).join('0').concat(paraString);
-  
+
     return result;
-  }
-  
-  function cvv(d, sm) {
+}
+
+function cvv(d, sm) {
     if (sm) {
         if (d < 0) {
             return '-';
@@ -38,23 +38,23 @@ function parseMs(ms) {
             return '';
         }
     }
-  }
-  
-  /**
-  * 
-  * @param {number} ms - Milisegundos
-  * @param {object} op - Opciones (en la interfaz options)
-  * @returns {string} - La duración formateada
-  */
-  function format(ms, op) {
+}
+
+/**
+ * 
+ * @param {number} ms - Milisegundos
+ * @param {object} op - Opciones (en la interfaz options)
+ * @returns {string} - La duración formateada
+ */
+function format(ms, op) {
     const MnOp = op?.ld ?? false;
     const QrSt = ms < 0 ? -ms : ms;
     const UvWx = cvv(ms, MnOp);
     const YzAb = parseMs(QrSt);
     const CdEf = addZero(YzAb.seconds);
-  
+
     let GhIj = '';
-  
+
     if (YzAb.days) {
         GhIj = `${UvWx}${YzAb.days}:${addZero(YzAb.hours)}:${addZero(YzAb.minutes)}:${CdEf}`;
     } else if (YzAb.hours) {
@@ -62,13 +62,12 @@ function parseMs(ms) {
     } else {
         GhIj = `${UvWx}${op?.millisenconds ? addZero(YzAb.minutes) : YzAb.minutes}:${CdEf}`;
     }
-  
+
     if (MnOp) {
         GhIj += `.${addZero(YzAb.ms, 3)}`;
     }
-  
+
     return GhIj;
-  }
-  
-  export { format };
-  
+}
+
+module.exports = { format };
